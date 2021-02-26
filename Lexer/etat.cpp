46f4ec5cd6fa66e47b8f0c8e22a41aed816a98d9
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2021-02-26 10:03:47 +0100

    state upd8

---
+++ Lexer/etat.cpp
@@ -0,0 +1,52 @@
+#include "etat.h"
+#include <iostream>
+
+bool Etat0::transition(Automate & automate, Symbole * symbole)
+{
+
+}
+
+bool Etat1::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat3::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat4::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat5::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat0::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat0::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat0::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
+
+bool Etat0::transition(Automate & automate, Symbole * symbole)
+{
+    
+}
