9cbe77d24ae7cddc31c4f5dad3a90d1440d588df
SayMouz <faouz5307@gmail.com>
2021-02-26 11:56:10 +0100

    correction Symbole Expr

--- Lexer/etat.h
+++ Lexer/etat.h
@@ -18,66 +18,66 @@ class Etat0 : public Etat
     public:
         Etat0();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat1 : public Etat 
 {
     public: 
         Etat1();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat2 : public Etat 
 {
     public:
         Etat2();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat3 : public Etat 
 {
     public:
         Etat3();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat4 : public Etat 
 {
     public:
         Etat4();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat5 : public Etat 
 {
     public:
         Etat5();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat6 : public Etat 
 {
     public:
         Etat6();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat7 : public Etat 
 {
     public:
         Etat7();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 
 class Etat8 : public Etat 
 {
     public:
         Etat8();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
 class Etat9 : public Etat 
 {
     public:
         Etat9();
         bool transition(Automate & automate, Symbole * symbole);
-}
+};
--- Lexer/symbole.h
+++ Lexer/symbole.h
@@ -9,54 +9,28 @@ const string Etiquettes[] = { "OPENPAR", "CLOSEPAR", "PLUS", "MULT", "INT", "FIN
 
 class Symbole {
    public:
-      Symbole(int i, bool t) : ident(i), terminal(t) {  }
+      Symbole(int i) : ident(i) {  }
       virtual ~Symbole() { }
       operator int() const { return ident; }
       virtual void Affiche();
-      bool estTerminal() {return terminal;}
+      bool estTerminal() {return ident != EXPR;}
+      void toExpr() {ident = EXPR;};
    protected:
       int ident;
-      bool terminal;
 };
 
 class Entier : public Symbole {
    public:
-      Entier(int v) : Symbole(INT, true), valeur(v) { }
+      Entier(int v) : Symbole(INT), valeur(v) { }
       ~Entier() { }
       virtual void Affiche();
    protected:
       int valeur;
 };
 
-class Expr : public Symbole {
+class Expr : public Entier {
    public :
-      Expr():Symbole(EXPR, false) {}
+      Expr(int v):Entier(v) {toExpr();}
       virtual ~Expr() {}
       virtual void Affiche();
-   protected :
-      // eval map
 };
-
-class ExprBin : public Expr {
-   public :
-      ExprBin(Expr * e1, Expr * e2):Expr(), expr1(e1), expr2(e2) {}
-      virtual ~ExprBin() {}
-   protected :
-      Expr * expr1;
-      Expr * expr2;
-};
-
-class ExprPlus : public ExprBin {
-   public :
-      ExprPlus(Expr * e1, Expr * e2):ExprBin(e1,e2), operateur(Symbole(PLUS,true)) {}
-   protected :
-      Symbole operateur;
-};
-
-class ExprMult : public ExprBin {
-   public :
-      ExprPlus(Expr * e1, Expr * e2):ExprBin(e1,e2), operateur(Symbole(MULT,true)) {}
-   protected :
-      Symbole operateur;
-};
-
